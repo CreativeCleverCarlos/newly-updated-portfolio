23d3665f96760deba53ae1c2532efce2b04f1218
Carlos <carlos.g.campbell@gmail.com>
2024-08-15 21:20:18 -0400

    everything but pictures

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
+    "name": "Carlos Campbell Website",
+    "email": "carlos.g.campbell@gmail.com",
     "phoneForTel": "555-779-4407",
     "phoneFormatted": "(555) 779-4407",
     "address": {
