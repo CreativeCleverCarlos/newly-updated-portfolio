f90a9268449e290c3a5d9ffe4b0183d2ef7541a8
Carlos <carlos.g.campbell@gmail.com>
2024-10-09 15:34:39 -0400

    edited meta

--- src/_data/client.js
+++ src/_data/client.js
@@ -16,5 +16,5 @@ module.exports = {
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "www.carloscampbell.com"
+    "domain": "https://carloscampbell.netlify.app/"
 };
